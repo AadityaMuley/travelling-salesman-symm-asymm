178750cbb26087416cc87b66a527638d69762952
AadityaMuley <aadityamuley8190@gmail.com>
2022-10-12 21:10:07 -0700

    Class added to calculate euclidian distance for symmetric data.

---
+++ SymmetricDistanceCalculate.java
@@ -0,0 +1,26 @@
+import java.lang.Math;
+
+public class SymmetricDistanceCalculate {
+
+    private float euclidianDistance(float x1, float y1, float x2, float y2) {
+        
+        float euclidianDistance = (float)Math.sqrt((Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2)));
+
+        return(euclidianDistance);
+    }
+
+    public float[][] calculateDistances(int arrLen, float[][] coords) {
+        float[][] symmetricDistances = new float[arrLen][arrLen];
+        float tempDist;
+
+        for(int i=0; i<arrLen-1; i++) {
+            for(int j=i+1; j<arrLen; j++){
+                tempDist = euclidianDistance(coords[i][0], coords[i][1], coords[j][0], coords[j][1]);
+                symmetricDistances[i][j] = tempDist;
+                symmetricDistances[j][i] = tempDist;
+            }
+        }
+
+        return(symmetricDistances);
+    }
+}
--- SymmetricMain.java
+++ SymmetricMain.java
@@ -3,16 +3,27 @@ import java.io.File;
 public class SymmetricMain {
     private int arrLen;
 	private float[][] coords;
+	private float[][] symmetricDistances;
 	private float[][] shortestPath;
 	private double totalDistance;
 
 	public void symmetricPanel() {
+
 		ParseSymmetricData parseSymmetricData = new ParseSymmetricData();
 		coords = parseSymmetricData.parseSymmData();
 		this.arrLen = parseSymmetricData.arrLen;
 		// for(int i=0; i<arrLen; i++) {
 		// 		System.out.println(i+1 + " " + coords[i][0] + " " + coords[i][1]);
 		// 	}
+
+		SymmetricDistanceCalculate symmetricDistanceCalculate = new SymmetricDistanceCalculate();
+		symmetricDistances = symmetricDistanceCalculate.calculateDistances(arrLen, coords);
+		for(int i=0; i<arrLen; i++) {
+			for(int j=0; j<arrLen; j++){
+				System.out.print(symmetricDistances[i][j] + "            ");
+			}
+			System.out.println();
+		}
 
 		TSPGreedy tspGreedy = new TSPGreedy();
 		shortestPath = tspGreedy.greedyShortestPath(arrLen, coords);
