cdabf79c6c27f1c7c0c832493ddd38437e1cc3ca
Jash Kahar <47451302+jashkahar@users.noreply.github.com>
2022-10-13 22:21:50 -0700

    Update ParseData.java

--- ParseData.java
+++ ParseData.java
@@ -1,8 +1,17 @@
 import java.io.File;
-
+/** ParseData is an Interface class 
+ * @author Aditya Mule, Manasi Anantpurkar, Jash Kahar, Sarthak Vats
+ */
 public interface ParseData {
     
+    /** Function getDimension is initialised here and needs to be implemented in a child class.
+     * @params file is the input file which contains the raw data.
+     */
     public int getDimension(File file);
-
+    
+    /** Function getCoords is initialised here and needs to be implemented in a child class.
+     * @param file is the input data file
+	 * @param arrLen is the dimension of file.
+     */
     public float[][] getCoords(File file, int arrLen);
 }
